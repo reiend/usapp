f69e2a031c0b09a23ef45c03bbe9e0e2fd9e8b6e
reiend <loncerasjakerei@gmail.com>
2022-06-15 13:22:26 +0800

    added authentication signin

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "@styles/usapp/fonts.scss";
 import "@styles/usapp/index.scss";
 
 import React, { Fragment } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {HashRouter, Routes, Route } from "react-router-dom";
 import Landing from "@components/Landing";
 import Signup from "@components/Authentication/Signup.jsx";
 import Chat from "@components/Chat";
@@ -11,13 +11,13 @@ import Chat from "@components/Chat";
 const App = () => {
   return (
     <Fragment>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="signup" element={<Signup />} />
           <Route path="chat" element={<Chat />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </Fragment>
   );
 };
